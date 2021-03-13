2b7d1e658d3b3c00ab25b0e6dfba7edc43ea419b
Charlee Li <oda.charlee@gmail.com>
2021-03-13 17:03:24 -0500

    added gunicorn

---
+++ gunicorn.py
@@ -0,0 +1,19 @@
+# vim: ft=python
+
+import os
+import multiprocessing
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RUNDIR = os.path.join(BASEDIR, 'run')
+LOGDIR = os.path.join(BASEDIR, 'log')
+
+bind = '0.0.0.0:18000'
+workers =multiprocessing.cpu_count() * 2
+chdir = BASEDIR
+pythonpath = BASEDIR
+daemon = False
+pidfile = os.path.join(RUNDIR, 'ocs.pid')
+
+loglevel = 'info'
+accesslog = os.path.join(LOGDIR, 'access.log')
+errorlog = os.path.join(LOGDIR, 'error.log')
